93825f0e890c6afeee7aa9f9bce43d83d8e78499
Peter Kong <bansuki@gmail.com>
2017-10-05 16:55:56 -0500

    added todo for routes

--- server.js
+++ server.js
@@ -7,16 +7,41 @@ const mongoose = require('mongoose'),
 app.use(bodyParser.json());
 
 // routes
+// TODO move all these callbacks to a routes dir
 app.get('/', function(req, res) {
   res.send('you are at root');
+});
+
+app.get('/trackers', function(req, res) {
+  db.MealTracker.find({}, function(err, trackers) {
+    if(err) {
+      res.status(500).send('error retrieving trackers');
+    } else {
+      res.json(trackers);
+    }
+  })
+});
+
+app.get('/meals', function(req, res) {
+  db.MealEntry.find({}, function(err, entries) {
+    if(err) {
+      res.status(500).send('error retrieving meal entries');
+    } else {
+      res.json(entries);
+    }
+  })
 })
 
 app.get('/foods', function(req, res) {
   db.Food.find({}, function(err, foods) {
-    res.json(foods);
+    if(err) {
+      res.status(500).send('error retrieving foods');
+    } else {
+      res.json(foods);
+    }
   })
-})
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
-})
+});
